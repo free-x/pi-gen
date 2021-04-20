c680ed8041bfa3f6c7167094c6cfa212c6351aae
free-x <oroitburd@gmail.com>
2021-04-20 22:11:20 +0200

    Use RELEASE as configurable variable in stage-avnav and stage-sk

--- stage-avnav/00-add-sources/00-run.sh
+++ stage-avnav/00-add-sources/00-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 install -m 644 files/extra.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
+sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/extra.list"
 
 on_chroot apt-key add - < files/oss.boating.gpg.key
 on_chroot apt-key add - < files/open-mind.space.gpg.key
--- stage-sk/00-add-sources/00-run.sh
+++ stage-sk/00-add-sources/00-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 install -m 644 files/skextra.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
+sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/skextra.list"
 
 on_chroot apt-key add - < files/nodesource.gpg.key
 
